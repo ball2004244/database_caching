f587193ae6104bb1956a6f22d3adefc9754cdf72
ball712004 <nguyenminhtam7124@gmail.com>
2023-04-04 02:12:12 -0400

    Try to integrate ReadySet DB

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
 from caching import redis_client
 
 # add user to database
-key = f'user:{11}'
-redis_client.add_user_to_redis(key, 'Jack', 'Jakarta', '08123456789')
+key = f'user:{20}'
+# redis_client.add_user_to_redis(key, 'Jack', 'Jakarta', '08123456789')
+redis_client.add_user_to_redis(key, 'New_USER', 'Vietnam', '1234')
 
 # get user from database
 # user = redis_client.get_all_users_from_redis()
